67cb87336454cbb65d33a1048b6e5c87c2727799
GautamAgnihotri <78820106+GautamAgnihotri@users.noreply.github.com>
2021-03-05 09:44:55 -0800

    creating frame by extending frame class
    creating frame by extending frame class

---
+++ frame2.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+class frame2 extends Frame
+{   frame2(){
+        setVisible(true);
+        setSize(800,1000);
+        setTitle("Extends Frame class");
+
+}
+public static void main(String[] args) {
+    frame2 fr = new frame2();
+}
+    
+}
+
+
+
