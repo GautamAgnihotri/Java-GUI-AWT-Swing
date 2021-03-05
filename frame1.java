bb4f52f4ef191301c64f29a54168d4a387f6b80f
GautamAgnihotri <78820106+GautamAgnihotri@users.noreply.github.com>
2021-03-05 09:43:24 -0800

    Add files via upload

---
+++ frame1.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+class frame1
+{
+     public static void main(String[] args) {
+         Frame f1=new Frame("MY First FRAME");
+         
+         f1.setSize(500,500);
+         f1.setVisible(true);
+        
+    }
+}
